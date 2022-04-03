d84bfefb68075e231bcbdeff17c7621a4d2839b5
Kunal Sharma <kunal.sharma@firstinsight.com>
2022-04-03 16:44:41 +0530

    update word list component

--- frontend/src/app/word-list/word-list.component.ts
+++ frontend/src/app/word-list/word-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {WordService} from "../core/services/word.service";
 import {Word} from "../shared/models/word";
+import {AddEditWordDialogService} from "../core/services/add-edit-word-dialog.service";
 
 @Component({
   selector: 'app-word-list',
@@ -11,12 +12,22 @@ export class WordListComponent implements OnInit {
 
   words: Word[] = []
 
-  constructor(private wordService: WordService) {
+  constructor(private wordService: WordService, private addEditWordDialogService: AddEditWordDialogService) {
   }
 
   ngOnInit(): void {
     this.wordService.getAllWords().subscribe((words) => {
       this.words = words;
     })
+  }
+
+  addWord(): void {
+    console.log('clicked');
+    this.addEditWordDialogService.openDialog().afterClosed().subscribe((word: Word) => {
+      console.log(word);
+      if (word && word.content) {
+        this.wordService.addWord(word).subscribe(word => this.words.push(word));
+      }
+    });
   }
 }
