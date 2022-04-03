50c1cdc58c505d4be167cc86c0254e540c0213e5
Kunal Sharma <kunal.sharma@firstinsight.com>
2022-04-03 17:18:20 +0530

    update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import {AppComponent} from './app.component';
 import {WordListComponent} from './word-list/word-list.component';
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core/core.module";
+import {WordCardComponent} from './word-list/word-card/word-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordListComponent
+    WordListComponent,
+    WordCardComponent
   ],
   imports: [
     CoreModule,
