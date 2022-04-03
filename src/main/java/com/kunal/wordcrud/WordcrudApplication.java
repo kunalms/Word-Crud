e0614fec758d6f9fd47d98d1121346a9286a5676
Kunal Sharma <kunal.sharma@firstinsight.com>
2022-04-03 13:03:27 +0530

    add git VCS

---
+++ src/main/java/com/kunal/wordcrud/WordcrudApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.wordcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcrudApplication.class, args);
+	}
+
+}
